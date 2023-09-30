c50d0475fe689fc21f531388ce991aab6bcfa1a1
matipasse <matiaspasserini51@gmail.com>
2023-09-30 20:25:04 -0300

    comienzo

---
+++ js.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let boton = document.getElementById('boton');
+    
+    boton.addEventListener('click', () => {
+        let nombrePokemon = document.getElementById('pokemonId').value;
+        let url = `https://pokeapi.co/api/v2/pokemon/${nombrePokemon}`;
+    
+    
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                let nombre = data.name;
+                let imagen = data.sprites.front_default;
+
+    
+                let resultadoHTML = `
+                    
+                    <img src="${imagen} id"imagen" alt="${nombre}">
+                    <p id="nombre">${nombre}<p>
+
+                `;
+    
+                document.getElementById('resultado').innerHTML = resultadoHTML;
+            })
+            .catch(error => {
+                console.error('Error al obtener datos del Pokémon:', error);
+                document.getElementById('resultado').innerHTML = 'No se pudo encontrar el Pokémon.';
+            });
+    });
+  });
+
+
